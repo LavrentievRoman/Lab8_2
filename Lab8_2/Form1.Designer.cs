de48774812beca1dd13c2e96296a8f2a0f518dba
KusokGoracio <lawrentiev.roma@gamil.com>
2024-04-10 18:25:47 +0700

    Добавьте файлы проекта.

---
+++ Lab8_2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Lab8_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.AutoSize = true;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answerLabel.Location = new System.Drawing.Point(7, 103);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(0, 29);
+            this.answerLabel.TabIndex = 5;
+            // 
+            // answerButton
+            // 
+            this.answerButton.Location = new System.Drawing.Point(101, 43);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(107, 47);
+            this.answerButton.TabIndex = 4;
+            this.answerButton.Text = "Ответ";
+            this.answerButton.UseVisualStyleBackColor = true;
+            this.answerButton.Click += new System.EventHandler(this.answerButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 153);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.answerButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab8_2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+            double[] p = new double[8] { 0.1, 0.1, 0.15, 0.15, 0.15, 0.15, 0.1, 0.1 };
+            string[] pred = new string[8] { "Бесспорно", "Никаких сомнений", "Вероянтее всего", 
+                                            "Хорошие перспективы", "Спроси позже", "Лучше не рассказывать",
+                                            "Даже не думай", "Весьма сомнительно"};
+
+            double a = rnd.NextDouble();
+
+            for (int i = 0; i < 8; i++)
+            {
+                a -= p[i];
+                if (a <= 0)
+                {
+                    answerLabel.Text = pred[i];
+                    break;
+                }
+            }
+        }
+    }
+}
